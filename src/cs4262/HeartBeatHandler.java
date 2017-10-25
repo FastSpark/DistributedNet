376e10bf290fe5e9be247507cabd4d775f70dcb2
Nuwantha <nuwanthadew@gmail.com>
2017-10-25 14:53:17 +0530

    heartbeathandler is added

---
+++ src/cs4262/HeartBeatHandler.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4262;
+
+import java.net.DatagramSocket;
+
+/**
+ *
+ * @author nuwantha
+ */
+public class HeartBeatHandler implements Runnable{
+    private DatagramSocket ds;
+    private Client client;
+    public HeartBeatHandler(Client client, DatagramSocket ds){
+        this.client=client;
+        this.ds=ds;
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            
+        }
+    }
+    
+}
